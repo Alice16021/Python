04dcd59dd20723cbc0e98aac323200d37412d07c
Alice Maria <72365221+Alice16021@users.noreply.github.com>
2026-02-18 09:12:41 -0300

    Add user input and check for names and phone numbers

---
+++ Consultas.py
@@ -0,0 +1,14 @@
+Nomes = ["Ivete Francisca","Bianca Caroline","Maria Alice"]
+Telefone = ["38345421","38346190","19982574"]
+Nome = input("Por favor coloque seu nome: ")
+Telefone = input("Por favor coloque seu telefone: ")
+print (Nome, Telefone)
+print("Ivete Francisca" in Nomes)
+print("Bianca Caroline" in Nomes)
+print("Maria Alice" in Nomes)
+print("38345421" in Telefone)
+print("38346190" in Telefone)
+print("19982574" in Telefone)
+
+Usuários = Nomes + Telefone
+print (Usuários)
